fc26624a199aacf2e407efebe9cc2543238db3a6
Mingala <lcambou@wanadoo.fr>
2018-08-17 16:48:24 +0100

    Update docker_entrypoint.sh

--- usr/local/bin/docker_entrypoint.sh
+++ usr/local/bin/docker_entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # setup MariaDB if no mysql database found
 if [ ! -f "$MYSQL_DATABASE/my.sql" ]; then
-	echo "setup MariaDB in $MYSQL_DATABASE"
+	echo "INFO: setup MariaDB in $MYSQL_DATABASE"
 	/usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-auth-anonymous-user
 	echo "WARN: MariaDB root user created without password"
 	echo "WARN: change it using SQL client: ALTER USER root IDENTIFIED BY 'something'"
 fi
 # run mysql database
-/usr/bin/mysqld_safe --defaults-extra-file=/etc/mysql/extra/my.cnf --user=mysql --datadir=/var/lib/mysql
+echo "INFO: start MariaDB with extra config file in $MYSQL_DATABASE"
+/usr/bin/mysqld_safe --defaults-extra-file=/etc/mysql/extra/my.cnf --user=mysql --datadir=$MYSQL_DATABASE
