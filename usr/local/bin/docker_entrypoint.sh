77f8c97231261d1122e49af01d6fa296278947d0
Mingala <lcambou@wanadoo.fr>
2018-08-18 21:10:35 +0100

    Update docker_entrypoint.sh

--- usr/local/bin/docker_entrypoint.sh
+++ usr/local/bin/docker_entrypoint.sh
@@ -4,4 +4,4 @@ if [ ! -d "$MYSQL_DATABASE/mysql" ]; then
 	/usr/bin/mysql_install_db --user=mysql --datadir=$MYSQL_DATABASE --skip-auth-anonymous-user
 fi
 # run mysql database
-/usr/bin/mysqld_safe --defaults-extra-file=$MYSQL_EXTRA/my.cnf --user=mysql --datadir=$MYSQL_DATABASE &
+/usr/bin/mysqld_safe --defaults-extra-file=$MYSQL_EXTRA/my.cnf --user=mysql --datadir=$MYSQL_DATABASE
