ad63b264deaaa56546fc1086f6a4a927b14d06ca
Mingala <lcambou@wanadoo.fr>
2018-09-23 18:10:30 +0100

    Update docker_entrypoint.sh
    mysql_install_db parameters change in 10.3

--- usr/local/bin/docker_entrypoint.sh
+++ usr/local/bin/docker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 # setup MariaDB if no mysql database found
 if [ ! -d "$MYSQL_DATABASE/mysql" ]; then
-	/usr/bin/mysql_install_db --user=mysql --datadir=$MYSQL_DATABASE --skip-auth-anonymous-user
+	/usr/bin/mysql_install_db --user=mysql --datadir=$MYSQL_DATABASE --skip-test-db
 fi
 # run mysql database
 /usr/bin/mysqld_safe --defaults-extra-file=$MYSQL_EXTRA/my.cnf --user=mysql --datadir=$MYSQL_DATABASE
